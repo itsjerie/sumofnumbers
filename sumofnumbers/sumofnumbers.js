8632c9c75a721fefc4c26d092dc336172147ea82
Jeraldine Milla <jeraldinemilla@gmail.com>
2022-02-06 19:46:31 -1000

    Update sumofnumbers.js
    committing second try of sumofnumbers to repo

--- sumofnumbers/sumofnumbers.js
+++ sumofnumbers/sumofnumbers.js
@@ -1,38 +1,34 @@
-const testData = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const testData = [1, 2, 3, 4];
 
-function sumFor(data) {
+function sumFor(data){
   let total = 0;
-  for (const val of data) {
-    total += val;
+  for(const num of data){
+    total += num;
   }
   return total;
 }
-
 console.log(sumFor(testData));
 
-function sumWhile(data) {
+function sumWhile(data){
   let total = 0;
-  let i = 0;
-  while (i < data.length) {
+  let i =0;
+  while(i< data.length){
     total += data[i];
     i++;
   }
   return total;
 }
-
 console.log(sumWhile(testData));
 
-function sumRecursion(data) {
-  if (data.length === 0) {
+function sumRecursion(data){
+  if(data.length === 0){
     return 0;
   }
-  return data[0] + sumRecursion(data.slice(1, data.length));
+  return sumRecursion(data.slice(1,data.length)) + data[0];
 }
-
 console.log(sumRecursion(testData));
 
-function sumTheSimpleWay(data) {
-  return _.reduce(data, function (memo, num) { return memo + num; }, 0);
+function sumTheSimpleWay(data){
+  return _.reduce(data, function(memo,num){return num+memo;},0);
 }
-
 console.log(sumTheSimpleWay(testData));
